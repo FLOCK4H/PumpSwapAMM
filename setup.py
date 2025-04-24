58d3318b9860cc8278f6f551ab01713448bb2b0e
FLOCK4H <flock4h@gmail.com>
2025-04-25 01:00:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 this_dir = Path(__file__).parent
 setup(
     name            = "PumpSwapAMM",
-    version         = "2.0.5",
+    version         = "2.0.6",
     description     = "Python SDK + optional CLI for Pump.fun’s PumpSwap AMM on Solana",
     long_description= (this_dir / "README.md").read_text(),
     long_description_content_type="text/markdown",
